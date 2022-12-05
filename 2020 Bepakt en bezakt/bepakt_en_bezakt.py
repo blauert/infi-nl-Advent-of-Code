72a424f26c3d9f5765792a3b681638aa00939baf
blauert <95693202+blauert@users.noreply.github.com>
2022-12-05 15:09:30 +0100

    Use proper bisection

--- 2020 Bepakt en bezakt/bepakt_en_bezakt.py
+++ 2020 Bepakt en bezakt/bepakt_en_bezakt.py
@@ -1,47 +1,52 @@
 # https://aoc.infi.nl/2020/
 
-def side_count(goal, start=1, step=1):
-    side_length = start
-    if step == 0:
-        print("Error! Step size should not be 0.")
-        return 0
-    while True:
-        pakjes = 0
-        pakjes_curr = side_length
-        for i in range(side_length):
-            pakjes += pakjes_curr
-            pakjes_curr += 2
-        middle = side_length * pakjes_curr
-        pakjes *= 2
-        pakjes += middle
-        if step > 0 and pakjes >= goal:
-            return side_length
-        if step == -1 and pakjes <= goal:
-            return side_length - step
-        elif step < 0 and pakjes <= goal:
-            return side_length + step
-        side_length += step
+
+def aantal_pakjes_in_zak(side_length):
+    pakjes_curr = side_length
+    pakjes = 0
+    for i in range(side_length):
+        pakjes += pakjes_curr
+        pakjes_curr += 2
+    middle = side_length * pakjes_curr
+    pakjes *= 2
+    pakjes += middle
+    return pakjes
+
+
+def kleinste_zak(goal, step=1):
+    guess = {'low': 0, 'high': float('inf')}
+    # Guess
+    while guess['high'] == float('inf'):
+        curr = guess['low'] + step
+        aantal_pakjes = aantal_pakjes_in_zak(curr)
+        if aantal_pakjes < goal:
+            guess['low'] = curr
+        elif aantal_pakjes >= goal:
+            guess['high'] = curr
+    # Bisect
+    while guess['high'] > (guess['low'] + 1):
+        half = guess['high'] - (guess['high'] - guess['low']) // 2
+        aantal_pakjes = aantal_pakjes_in_zak(half)
+        if aantal_pakjes < goal:
+            guess['low'] = half
+        elif aantal_pakjes >= goal:
+            guess['high'] = half
+    return guess['high']
 
 
 # Deel 1
 
-pakjes_goal = 17485880
-side_len = side_count(pakjes_goal)
-
+pakjes_goal = 17488031
+side_len = kleinste_zak(pakjes_goal)
 print(f"Side length required for {pakjes_goal} pakjes: {side_len}")
 
 
 # Deel 2
 
-# Steps should alternate between consecutively decreasing positive and negative values,
-STEPS = [1000, -100, 10, -1]  # with the last step size being either 1, or -1.
-
 pakjes_goals = [4541682716, 1340798729, 747740862, 430796666, 368939948, 42728646]
 stukken_stof = 0
+
 for pakjes_goal in pakjes_goals:
-    best_guess = 1000
-    for step in STEPS:
-        best_guess = side_count(pakjes_goal, best_guess, step)
-    stukken_stof += best_guess * 8
+    stukken_stof += kleinste_zak(pakjes_goal, step=3000) * 8
 
 print(f"Stukken stof required for alle zakken: {stukken_stof}")
